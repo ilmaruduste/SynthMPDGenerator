43ee3ebcf8f9b0aa32cfa77b5ff4764a883495aa
ilmaruduste <ilmaruduste@gmail.com>
2021-06-15 14:19:07 +0300

    Add first barebones user

---
+++ mpdgen/users.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+class User(ABC):
+
+    def __init__(self, id):
+        self.id = id
+        self.profile = "User"
+
+    def generateMeaningfulLocations(self):
+        print(f"Generating meaningful locations for user {self.id} of profile {self.profile}.")
+
+class GeneralUser(User):
+    # Use this for generalising a population
+    #     or if you don't want any special classes
+
+    pass
