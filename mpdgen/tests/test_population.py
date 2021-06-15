895d89354cc57d43c11e1634ca0c69424202c1d7
Ilmar Uduste <ilmar.uduste@positium.com>
2021-06-15 15:50:33 +0300

    Add .gitignore to ref_data folder

--- mpdgen/tests/test_population.py
+++ mpdgen/tests/test_population.py
@@ -8,7 +8,8 @@ def testPopulation():
 
     randomPopulation = population.Population(randomProfileDict)
 
-    print(randomPopulation.profile_dict)
+    # It is also possible to have the syntax as simply print(f"{randomPopulation.profile_dict=}"), but this feature is Python 3.8+ only and I'm on 3.7.4
+    print(f"randomPopulation.profile_dict = {randomPopulation.profile_dict}")
 
     randomPopulation.generatePopulation()
     randomPopulation.generateMeaningfulLocations()
