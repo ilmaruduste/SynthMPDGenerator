b94e6d98092c1fd5b869e35119bb5abc00f5db1a
Ilmar Uduste <ilmar.uduste@positium.com>
2021-06-17 15:59:12 +0300

    Add timing function to testing

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,16 @@
 from mpdgen.tests import test_users, test_population
 import yaml
+import time
+
+start_time = time.time()
 
 conf = yaml.safe_load(open("config.yaml", "r"))
 
+conf_time = time.time()
+print(f"Config time: {round(conf_time - start_time, 2)} seconds.")
 
 # test_users.testAbstractUser(cells_filepath)
 test_population.testPopulation(conf)
+
+conf_time = time.time()
+print(f"Test Population time: {round(conf_time - start_time, 2)} seconds.")
